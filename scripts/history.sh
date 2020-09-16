b830845cd601a1dd2c16251b264c5b8e79e6affc
Frank Göldner <frank.goeldner@myhotelshop.de>
2020-09-16 13:38:11 +0200

    installed required packages and created relevant directories

--- scripts/history.sh
+++ scripts/history.sh
@@ -7,3 +7,11 @@ git pull origin master
 
 # Initialize Go Modules
 go mod init github.com/f-go/petstore
+
+# Create required directories
+mkdir -p api/protobuf
+mkdir -p api/openapi
+mkdir -p pkg/petstore
+
+# Install Go required packages
+go get -u github.com/golang/protobuf/protoc-gen-go
